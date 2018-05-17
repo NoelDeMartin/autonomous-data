52fff82b3e8ef8dc1f30cc34c5ee78c493f60cc6
Noel De Martin <noeldemartin@gmail.com>
2018-05-17 13:26:15 +0200

    Update colors and add logos

--- site/.vuepress/config.js
+++ site/.vuepress/config.js
@@ -2,6 +2,12 @@ module.exports = {
   title: 'Autonomous Data Protocol',
   description: 'Let\'s make true data ownership a reality',
   base: '/autonomous-data/',
+  head: [
+    ['link', { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/icons/favicon-32x32.png' }],
+    ['link', { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/icons/favicon-16x16.png' }],
+    ['link', { rel: 'apple-touch-icon', sizes: '180x180', href: '/icons/apple-touch-icon.png' }],
+    ['meta', { name: 'theme-color', content: '#f9a825' }],
+  ],
   themeConfig: {
     repo: 'noeldemartin/autonomous-data',
     nav: [
--- site/README.md
+++ site/README.md
@@ -1,5 +1,12 @@
 ---
 home: true
+heroImage: /logo.svg
 actionText: Learn How →
 actionLink: /guide.html
 ---
+
+<style>
+    .home .hero img {
+        max-width: 85vw;
+    }
+</style>
